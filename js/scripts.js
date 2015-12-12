d4a6d37c881de0b3f2aaa6823306700e25074024
Vasyl Samusiev <catvaas@googlemail.com>
2015-12-12 20:49:47 +0200

    Main page & sign up + log in modals

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,31 @@
-/*--- MODEL ---*/
-
-
 jQuery(function($){
-/*--- EVENT HANDLER ---*/
-	
-	
-/*--- CONTROLLER ---*/
-	
-})
+	/*--- Modals ---*/
+	var modalSpeed = 200;
+	var modals = $('.modals');
+
+	modals.click(function(ev){
+		$(this).hide(modalSpeed);
+		$(this).children('.modals-box:visible').hide(modalSpeed);
+	});
+
+	$('.modals-box').click(function(ev){
+		ev.stopPropagation();
+	});
+
+	$('.modals-box-close').click(function(ev){
+		$(this).parent('.modals-box').hide(modalSpeed);
+		modals.click();
+	});
+
+	// Sign up & Log in
+	$('.header-login a').click(function(ev){
+		ev.preventDefault();
+
+		var modalId = $(this).attr('data-modal');
+
+		modals.show(modalSpeed);
+		$('#' + modalId + ':hidden').show(modalSpeed);
+
+		return false;
+	});
+});
