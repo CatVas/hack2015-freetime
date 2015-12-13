c62949a9a1918014dc99884d8acb234df33ae872
Vasyl Samusiev <catvaas@googlemail.com>
2015-12-13 04:27:58 +0200

    User tasks and add task form

--- js/scripts.js
+++ js/scripts.js
@@ -18,14 +18,55 @@ jQuery(function($){
 	});
 
 	// Sign up & Log in
-	$('.header-login a').click(function(ev){
+	$('.header-login a').
+	add('.tasks-add a').click(function(ev){
 		ev.preventDefault();
 
 		var modalId = $(this).attr('data-modal');
 
-		modals.show(modalSpeed);
-		$('#' + modalId + ':hidden').show(modalSpeed);
+		switch(modalId){
+			case 'login':
+			case 'signup':
+				modals.show(modalSpeed);
+				$('#' + modalId + ':hidden').show(modalSpeed);
+			break;
+		}
 
 		return false;
 	});
+	/*--- /Modals ---*/
+
+
+	/*--- Task form ---*/
+	$('.tasks-add [data-modal="add-task"]').click(function(ev){
+		ev.preventDefault();
+		//console.log('Форма додавання завдання');
+		$(this).slideUp(modalSpeed);
+		$(this).parents('.tasks-add').find('.tasks-add-form:hidden').slideDown(modalSpeed);
+	});
+
+	$('.tasks-add-form').submit(function(ev){
+		var th = this;
+
+		// Process the form fields values
+
+		$(th).slideUp(modalSpeed);
+		$(th).parents('.tasks-add').find('.tasks-add-success:hidden').slideDown(modalSpeed);
+
+		setTimeout(function(){
+			$(th).parents('.tasks-add').find('.tasks-add-success:visible').hide(0);
+			$(th).parents('.tasks-add').find('[data-modal="add-task"]:hidden').slideDown(modalSpeed);
+		}, 1000);
+
+		return false;
+	});
+	/*--- /Task form ---*/
+
+
+	/*--- Accordion ---*/
+	$('.tasks-list').accordion();
+
+
+	/*--- Datepicker ---*/
+	$('.tasks-add-form [name="task_deadline"]').datepicker({});
 });
